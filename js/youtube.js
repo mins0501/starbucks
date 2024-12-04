47a77c5f65d411db97ec24972b9f2e1440691fb9
Kim-MinSu <mins0501@gmail.com>
2024-12-04 11:02:43 +0900

    로그인 페이지 시작하기

--- js/youtube.js
+++ js/youtube.js
@@ -20,8 +20,8 @@ function onYouTubePlayerAPIReady() {
     events: {
       // 영상이 준비되었을 때,
       onReady: function (event) {
-        event.target.mute(); // 음소거!
+        event.target.mute() // 음소거!
       }
     }
-  });
-};
+  })
+}
